14529504ec5d20eb47b66e4f317d0d569f7d8bf6
Redone0001 <35457642+Redone0001@users.noreply.github.com>
2018-12-23 12:24:00 +0100

    Update fusee.cpp

--- src/fusee.cpp
+++ src/fusee.cpp
@@ -8,9 +8,8 @@
 
 using namespace std;
 
-fusee::fusee(int Gen, int BrainSize):Brain(BrainSize), gen(Gen) {
-	
-	//initialisationCSV("gen_"+to_string(gen));
+fusee::fusee(int Gen, int BrainSize):Brain(BrainSize), gen(Gen) { // constructeur de la fusée
+	// les modules ne sont pas ceux donné dans l'énoncé nous leur avons donné plus de carburant et plus de puissance
 	module soyuz(6930,70,0,2.86,0,0);
 	module etage1(6500,205000,1200000,3.42,350,0);
 	module etage2(2250,252000,400000,2.78,105,0);
@@ -21,16 +20,16 @@ fusee::fusee(int Gen, int BrainSize):Brain(BrainSize), gen(Gen) {
 		module booster(3500,44000,1200000,2.82,333.33,0);
 		lanceurVec.push_back(booster);
 	}
-	for(int i = 3;i<7;i++){
+	for(int i = 3;i<7;i++){ // on allume les booster, ils ne seront plus jamais éteint
 		lanceurVec.at(i).throttle=1;
 	}
 	lanceurVec.at(2).throttle=1;
 	vivant = true;
 	fit =0;
 }
-void fusee::nextStep(long double t){
+void fusee::nextStep(long double t){ // définition de l'évoluition de la fusée
 	if(vivant){
-		for (unsigned int moduleId = 0; moduleId < lanceurVec.size(); moduleId++){
+		for (unsigned int moduleId = 0; moduleId < lanceurVec.size(); moduleId++){ // vérification qu'il reste assez de carburant pour avancer sans utiliser du carburant qui n'existe pas
 			bool test = ENIAC.checkCarburant(lanceurVec.at(moduleId));
 			if (test && (lanceurVec.size()!=1))
 				lanceurVec.erase(lanceurVec.begin()+moduleId);
@@ -38,8 +37,7 @@ void fusee::nextStep(long double t){
 				ENIAC.updateCarburant(lanceurVec.at(moduleId));
 		
 		}
-		//cout<<"test0"<<endl;
-		int step = t/15;
+		int step = t/15; // on ne veut que les modification du cerveau n'ai lieu que toute les 15sec
 		if(lanceurVec.size()==3){
 			throttleGeneral = 2;
 		}
@@ -50,41 +48,34 @@ void fusee::nextStep(long double t){
 			throttleGeneral = 0;
 			
 		}
-		//cout<<"test1"<<endl;
 		int longeur = Brain.Angle.size();
 		bool test = step < longeur;
 		//cout <<test<<endl;
-		if (test){
+		if (test){ // on vérifie qu'il reste des instruction à éxécuter
 			if (throttleGeneral == 0)
 				Brain.Throttle.at(step)=0;
 			ENIAC.angle = Brain.Angle.at(step);
 			lanceurVec.at(throttleGeneral).throttle=Brain.Throttle.at(step);
 		}
-		//cout<<"test2"<<endl;
 		ENIAC.sumForce(lanceurVec);
 		long double masse = ENIAC.checkMasse(lanceurVec);
 		ENIAC.updateMouv(0.1,masse);
-		//cout<<"test2"<<endl;
 		if(sqrt(pow(ENIAC.position.first,2)+pow(ENIAC.position.second,2))<6370990){
 			vivant=false;
-			//cout <<"dead"<<endl;;
 		}
 		/*if((sqrt(pow(ENIAC.position.first,2)+pow(ENIAC.position.second,2))>6571000) & (sqrt(pow(ENIAC.position.first,2)+pow(ENIAC.position.second,2))<6971000)){
-			fit++;
+			fit++; // donne uné récompense pour être dans la bonne couche d'orbite
 		}*/
 	}
 
 }
 void fusee::calculFitness(){
-	//cout <<fit <<endl;
 	//fit= fit*ENIAC.checkMasse(lanceurVec)/10000;
 	fit = (sqrt(pow(ENIAC.position.first,2)+pow(ENIAC.position.second,2))-6371000);
 }
 
 fusee fusee::gimmeBaby(){
-	fusee baby(gen+1, Brain.Angle.size());
-// brain deuxiemeEssai(premierEssai);
-// brain deuxiemeEssai = premierEssai.clone();
+	fusee baby(gen+1, Brain.Angle.size()); // on fait faire des bébé aux fusées
 	baby.Brain = brain(Brain);//.clone();
 	return baby;
 
